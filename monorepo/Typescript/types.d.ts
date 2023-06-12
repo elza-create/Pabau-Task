3a480d0507cd77b0299a73ce5a4b7d5dff715ec8
Elza2000 <elza.aliijaj@gmail.com>
2023-06-12 14:45:43 +0200

    Elza2000: Add interfaces for Interns, Juniors, and Company

---
+++ monorepo/Typescript/types.d.ts
@@ -0,0 +1,27 @@
+interface Intern {
+  name: string;
+  age: number;
+  skills: string[];
+}
+
+declare enum Domain {
+  Web = "Web",
+  Connect = "Connect",
+  Backend = "Backend",
+}
+
+interface Junior {
+  name: string;
+  age: number;
+  skills: string[];
+  date_of_promotion: Date;
+  domain: Domain;
+}
+
+interface Company {
+  Interns: Intern[];
+  Juniors: Junior[];
+  Country: string;
+}
+
+export default Company;
